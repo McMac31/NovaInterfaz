76a849c4cf93d585d390fe78ba5c6d8062d15976
unknown <ikmacampo24>
2025-10-23 09:46:12 +0200

    Primera version subida sin funcionalidades boceto

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace InterfazNova
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AbrirCuadroVentas(object sender, MouseButtonEventArgs e)
+        {
+            NumVentasMes numVentas = new NumVentasMes();
+            numVentas.ShowDialog();
+        }
+
+        private void AbrirTotal(object sender, MouseButtonEventArgs e)
+        {
+            TotalFacturado totalFacturado = new TotalFacturado();
+            totalFacturado.ShowDialog();
+        }
+
+        private void AbrirPedidosPen(object sender, MouseButtonEventArgs e)
+        {
+            PedidosPendientes pedidosPendientes = new PedidosPendientes();
+            pedidosPendientes.ShowDialog();
+        }
+
+        private void AbrirStockBajo(object sender, MouseEventArgs e)
+        {
+            ProductosBajoStock productosBajoStock = new ProductosBajoStock();
+            productosBajoStock.ShowDialog();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ventanaClientes ventanaClientes = new ventanaClientes();
+            ventanaClientes.ShowDialog();
+        }
+    }
+}
+
